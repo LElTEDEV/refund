5237d40ce8302c5523a90d63b357cbc9cca610c5
igor <suporteaser2@gmail.com>
2025-04-16 21:57:10 -0300

    criado page SignIn com uma pequena funcionalidade de form

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+      <Input
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+      />
+
+      <Input
+        required
+        legend="Senha"
+        type="password"
+        placeholder="******"
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+      />
+
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
+    </form>
+  );
 }
