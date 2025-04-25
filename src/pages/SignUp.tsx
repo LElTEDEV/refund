2fc4fdde8ec4f165a771ae0a3409efe575c93d2b
LElTEDEV <zinenizi@gmail.com>
2025-04-25 10:44:51 -0300

    finalizado integração com o backend para realizar cadastro

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,25 @@
 import { useState } from "react";
+import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { api } from "../services/api";
+import { useNavigate } from "react-router";
+
+const signUpSchema = z
+  .object({
+    name: z.string().trim().min(1, { message: "Informe o nome" }),
+    email: z.string().email({ message: "E-mail inválido." }),
+    password: z
+      .string()
+      .min(6, { message: "Senha deve ter pelo menos 6 caracteres." }),
+    passwordConfirm: z.string({ message: "Confirme a senha" }),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "As senhas não são iguais",
+    path: ["passwordConfirm"],
+  });
 
 export function SignUp() {
   const [name, setName] = useState("");
@@ -9,8 +28,39 @@ export function SignUp() {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
+  const navigate = useNavigate();
+
+  async function onSubmit(e: React.FormEvent) {
     e.preventDefault();
+
+    try {
+      setIsLoading(true);
+
+      const data = signUpSchema.parse({
+        name,
+        email,
+        password,
+        passwordConfirm,
+      });
+
+      await api.post("/users", data);
+
+      if (confirm("Cadastrado com sucesso. Ir para tela de entrar?")) {
+        navigate("/");
+      }
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return alert(error.issues[0].message);
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Não foi possível cadastrar!");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
